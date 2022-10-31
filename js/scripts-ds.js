1a97bc3ec47beee4b21059eef92dbd8a7af5fe1a
YuuY314 <rafaelyu0202@gmail.com>
2022-10-31 18:13:59 -0300

    Update HTML/CSS

--- js/scripts-ds.js
+++ js/scripts-ds.js
@@ -3,6 +3,7 @@ const start = document.querySelector(".start-box");
 const menu = document.querySelector(".menu-option");
 const select = document.querySelectorAll(".select-btn");
 const content = document.querySelectorAll("#content");
+const bossCard = document.querySelectorAll(".boss-card");
 
 page.addEventListener("keydown", () => {
     start.classList.add("hide");
@@ -13,4 +14,10 @@ select.forEach(option => {
     option.addEventListener("click", () => content.forEach(container => {
         container.classList.remove("hide");
     }));
+});
+
+bossCard.forEach(boss => {
+    boss.addEventListener("click", () => {
+        alert("Ainda está em desenvolvimento")
+    });
 });
